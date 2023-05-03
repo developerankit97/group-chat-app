7fbde77f50d45ab7039266f33b52091928e68e41
developerankit97 <ankitsh.developer@gmail.com>
2023-05-03 21:35:38 +0530

    cron job added

--- app.js
+++ app.js
@@ -46,7 +46,49 @@ app.use(errorController.get404);
 
 sequelize
 	.sync()
-	.then(() => {
-		app.listen(process.env.PORT);
-	})
+	.then(() => {})
 	.catch((e) => console.log(e));
+
+var CronJob = require('cron').CronJob;
+var Job = new CronJob(
+	'1 1 3 * * *',
+	async function () {
+		//console.log(" Hello");
+		const allChats = await chats.findAll({
+			attributes: ['message', 'userId', 'groupId', 'createdAt'],
+		});
+		console.log('allchats type is ', allChats);
+		console.log(
+			'allchats are ====> ',
+			JSON.parse(JSON.stringify(allChats))
+		);
+
+		allChats.forEach((msg) => {
+			const durationofmsg =
+				new Date().getTime() - msg.createdAt.getTime();
+			console.log('Date ==> createdAt format : ', durationofmsg);
+			const hour = Math.ceil(durationofmsg / (1000 * 60 * 60));
+			if (hour < 24) {
+				moveMsgToArchievedChat(msg);
+				chats.destroy({
+					where: {
+						message: msg.message,
+						userId: msg.userId,
+						groupId: msg.groupId,
+					},
+				});
+			}
+		});
+
+		async function moveMsgToArchievedChat(msg) {
+			const response = await archivedChat.create({
+				message: msg.message,
+				userId: msg.userId,
+				groupId: msg.groupId,
+			});
+		}
+	},
+	null,
+	true,
+	'America/Los_Angeles'
+);
